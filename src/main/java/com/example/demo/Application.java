1b3401a7610c05d8a92410624d082a8cb5dd5a7c
ryan-rao <ryan-rao@qq.com>
2020-05-17 09:36:59 +0800

    Update Application.java

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class Application {
 
-	private static String title = "World Bank";
+	private static String title = "it is my World Bank";
 
 	public static String getTitle() {
 		return title;
